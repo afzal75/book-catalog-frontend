b2df3b43f61127d6d07e638033240d5f651bf7d8
afzal75 <afzalbhuiyan16@gmail.com>
2023-07-18 11:55:30 +0600

    hero section created successfully

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
-import routes from './router/routes';
+import { routes } from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
